f6cb5e5869b4641cfa3fefe78242566f5c5dfa25
programmerXrl <102668048+programmerXrl@users.noreply.github.com>
2023-12-12 20:48:14 +0800

    2023/12/12

---
+++ Main12.java
@@ -0,0 +1,19 @@
+package December;
+
+import July.ListNode;
+
+public class Main12 {
+    public static void main(String[] args) {
+
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
