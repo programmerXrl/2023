3b4b519e528831081e0eb2807a192cb2e634ed1e
programmerXrl <102668048+programmerXrl@users.noreply.github.com>
2023-12-27 22:03:49 +0800

    loser

---
+++ Main27.java
@@ -0,0 +1,23 @@
+package December;
+
+import July.TreeNode;
+
+public class Main27 {
+    public static void main(String[] args) {
+
+    }
+    long pre = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!isValidBST(root.left)){
+            return false;
+        }
+        if (root.val <= pre) {
+            return false;
+        }
+        pre = root.val;
+        return isValidBST(root.right);
+    }
+}
